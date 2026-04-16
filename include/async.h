188ab17702bba5942ea1bdfe2cfb9475cebe31b6
Jessica Mutta <>
2026-04-16 16:40:55 +0200

    added various comments and improved reaping logic

--- include/async.h
+++ include/async.h
@@ -11,40 +11,43 @@ typedef void* async_arg;
 typedef async_result (*async_callback)(async_arg);
 
 typedef struct {
-	pthread_cond_t done_signal;
-	_Atomic bool done;
-	async_callback callback;
-	async_arg arg;
-	async_result ret;
+	_Atomic bool done; // signal that the task has finished execution, therefore freeing the worker
+	_Atomic bool consumed; // signal that the task has been consumed and is ready for freeing
+	async_callback callback; // the worker function
+	async_arg arg; // the arg for the worker function
+	async_result ret; // the result of the returning callback function
+	size_t id; // the id of the task
 } async_task;
 typedef async_task *async_future;
 
 typedef struct {
-	async_task **data;
-	size_t size;
-	_Atomic size_t in;
-	_Atomic size_t out;
-	size_t mask;
+	async_task **data; // queue data
+	size_t size; // size of the queue in bytes
+	_Atomic size_t in; // first free slot
+	_Atomic size_t out; // first used slot
+	_Atomic size_t work_index; // first slot that needs a worker
+	size_t mask; // mask for the kfifo trick of infinite indexing
 	
-	pthread_mutex_t lock;
-	pthread_cond_t not_empty;
+	pthread_mutex_t lock; // queue lock
+	pthread_cond_t not_empty; // signal for the queue receiving new data
+	pthread_cond_t task_done; // signal for a task being completed
 } async_task_queue;
 
 typedef struct {
-	bool active;
+	bool active; // the runtime is active
+	async_task_queue queue; // the runtime queue
 
-	async_task_queue queue;
-
-	uint16_t thread_count;
-	pthread_t *thread_pool;
+	uint16_t thread_count; // worker thread count
+	pthread_t *thread_pool; // thw working threads
 
 } async_runtime_core;
 typedef async_runtime_core* async_runtime;
 
-
 async_runtime async_create_runtime(uint16_t thread_count);
 void async_destroy_runtime(async_runtime runtime);
+
 async_future async_dispatch(async_runtime runtime, async_callback function, void *arg); 
 async_result async_await(async_runtime runtime, async_future future);
+bool async_is_done(async_runtime runtime, async_future future, async_result *result);
 
 #endif /* __ACYNC_ASYNC_H */
--- src/async.c
+++ src/async.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <stdlib.h>
 
+// worker thread function
 static void *worker_thread(void *arg) {
 	async_runtime runtime = (async_runtime)arg;
 	async_task_queue *queue = &runtime->queue;
@@ -10,18 +11,26 @@ static void *worker_thread(void *arg) {
 	while(true) {
 		pthread_mutex_lock(&queue->lock);
 
-		while(queue->in == queue->out) {
+		// while queue empty, wait
+		while(queue->in == queue->work_index) {
 			pthread_cond_wait(&queue->not_empty, &queue->lock);
 		}
 
-		async_task *task = queue->data[queue->out & queue->mask];
-		++queue->out;
+		// gets the task to execute
+		async_task *task = queue->data[queue->work_index & queue->mask];
+		++queue->work_index;
 
 		pthread_mutex_unlock(&queue->lock);
 
+		// runs the task
 		task->ret = task->callback(task->arg);
 
 		task->done = true;
+		
+		// signal task is done
+		pthread_mutex_lock(&queue->lock);
+		pthread_cond_signal(&queue->task_done);
+		pthread_mutex_unlock(&queue->lock);
 	}
 
 	return NULL;
@@ -33,21 +42,21 @@ async_runtime async_create_runtime(uint16_t thread_count) {
 	async_runtime runtime = (async_runtime)malloc(sizeof(async_runtime_core));
 	assert(runtime != NULL);
 
-
 	// initialize the queue
 	async_task_queue *queue = &runtime->queue;
-	queue->size = 1024 * 1024;
-	queue->mask = queue->size - 1;
-	queue->in = queue->out = 0;
+	queue->size = 1024 * 1024; // chosen statically for now
+	queue->mask = queue->size - 1; // following the lead of kfifo
+	queue->in = queue->out = queue->work_index = 0;
 	queue->data = (async_task**)malloc(queue->size / sizeof(async_task*));
 	pthread_mutex_init(&queue->lock, NULL);
 
-
+	// setting up the thread pool
 	runtime->active = true;
 	runtime->thread_count = thread_count;
 	runtime->thread_pool = (pthread_t*)malloc(thread_count * sizeof(pthread_t));
 	assert(runtime->thread_pool != NULL);
 
+	// strating the thread pool
 	for (uint16_t thread = 0; thread < thread_count; ++thread) {
 		int ret = pthread_create(&runtime->thread_pool[thread], NULL, worker_thread, (void*)runtime);
 		assert(ret == 0);
@@ -70,32 +79,84 @@ async_future async_dispatch(async_runtime runtime, async_callback function, void
 
 	pthread_mutex_lock(&queue->lock);
 
+	// check if the queue is full
 	if ((queue->in - queue->out) == queue->size) {
 		pthread_mutex_unlock(&queue->lock);
 		assert(0 != 0);//panic, queue full
 	}
 
+	// create new task to dispatch
 	async_task *task = (async_task*)malloc(sizeof(async_task));
-	task->done = false;
+	task->done = task->consumed = false;
 	task->callback = function;
 	task->arg = arg;
-	queue->data[queue->in & queue->mask] = task;
+	task->id = queue->in;
+	queue->data[queue->in & queue->mask] = task; // indexing trick
 	++queue->in;
 
+	// signaling that a new task is ready
 	pthread_cond_signal(&queue->not_empty);
 	pthread_mutex_unlock(&queue->lock);
 
 	return task;
 }
 
-async_result async_await(async_runtime runtime, async_future future){
+static void async_free_backlog(async_task_queue *queue) {
+	// free as many queue slots as possible
+	while (queue->in != queue->out) {
+		async_task *oldest = queue->data[queue->out & queue->mask];
+
+		if (oldest && oldest->done && oldest->consumed) {
+			// if the oldest task is done and reaped, remove it
+			queue->data[queue->out & queue->mask] = NULL;
+			free(oldest);
+			++queue->out;
+		} else {
+			// a task hasnt been reaped yet, tbd
+			break;
+		}
+	}
+}
+
+async_result async_await(async_runtime runtime, async_future future) {
+	async_task_queue *queue = &runtime->queue;
+
+	// wait for the task to be done
 	async_task *task = future;
 	while(!task->done) {
-
+		pthread_cond_wait(&queue->task_done, &queue->lock);
 	}
 
+	// result is obtained
 	async_result ret = task->ret;
-	free(task);
+	task->consumed = true;
+
+	async_free_backlog(queue);
+
+	pthread_mutex_unlock(&queue->lock);
 
 	return ret;
+}
+
+bool async_is_done(async_runtime runtime, async_future future, async_result *result) {
+	async_task *task = future;
+
+	if(!task->done) {
+		// if the task isnt done, just return
+		return false;
+	}
+	
+	// task is done, lock the queue and extract the value
+	async_task_queue *queue = &runtime->queue;
+	pthread_mutex_lock(&queue->lock);
+
+	// result is obtained
+	if (result) *result = task->ret;
+	task->consumed = true;
+
+	async_free_backlog(queue);
+
+	pthread_mutex_unlock(&queue->lock);
+
+	return true;
 }
